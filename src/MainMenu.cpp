81f0ca11c53e382cd85a3881e1c3ad5205d3f320
Nicola Preda <nicola.preda05@gmail.com>
2022-02-16 13:00:31 +0100

    Re-Added Library

---
+++ src/MainMenu.cpp
@@ -0,0 +1,85 @@
+#include "MainMenu.h"
+
+MainMenu::MainMenu(float width, float height)
+{
+	// require font
+	if (!font.loadFromFile("../../env/windows/fonts/AlexandriaFLF.ttf"))
+	{
+		cout << "No fonts loaded";
+	}
+
+	// play
+	mainMenu[0].setFont(font);
+	mainMenu[0].setFillColor(Color::White);
+	mainMenu[0].setString("Play");
+	mainMenu[0].setCharacterSize(70);
+	mainMenu[0].setPosition(400, 200);
+
+	// options
+	mainMenu[1].setFont(font);
+	mainMenu[1].setFillColor(Color::White);
+	mainMenu[1].setString("Options");
+	mainMenu[1].setCharacterSize(70);
+	mainMenu[1].setPosition(400, 300);
+
+	// about
+	mainMenu[2].setFont(font);
+	mainMenu[2].setFillColor(Color::White);
+	mainMenu[1].setString("About");
+	mainMenu[2].setcharacterSize(70);
+	mainMenu[2].setPosition(400, 400);
+
+	// exit
+	mainMenu[2].setFont(font);
+	mainMenu[2].setFillColor(Color::White);
+	mainMenu[1].setString("Exit");
+	mainMenu[2].setcharacterSize(70);
+	mainMenu[2].setPosition(400, 500);
+
+	MainMenuSelected = -1;
+}
+
+MainMenu::~MainMenu()
+{
+}
+
+// draw mainMenu
+void MainMenu::draw(RenderWindow& window)
+{
+	for (int i = 0; i < Max_main_menu; i++)
+	{
+		window.draw(mainMenu[i]);
+	}
+}
+
+// move up
+void MainMenu::MoveUp()
+{
+	if (MainMenuSelected = -1 >= 0)
+	{
+		mainMenu[MainMenuSelected].setFillColor(Color::White);
+
+		MainMenuSelected--;
+		if (MainMenuSelected == -1)
+		{
+			MainMenuSelected = 2;
+		}
+		mainMenu[MainMenuSelected].setFillColor(Color::Blue);
+	}
+}
+
+// move down
+void MainMenu::MoveDown()
+{
+	if (MainMenuSelected + 1 <= Max_main_menu)
+	{
+		mainMenu[MainMenuSelected].setFillColor(Color::White);
+
+		MainMenuSelected++;
+		if (MainMenuSelected == 4)
+		{
+			MainMenuSelected = 0;
+		}
+		mainMenu[MainMenuSelected].setFillColor(Color::Blue);
+	}
+}
