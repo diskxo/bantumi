fe2a861aa3b33c0921998c49891f477391439a94
Nicola Preda <preda.nicola.studente@itispaleocapa.it>
2022-02-23 00:18:41 +0100

    🛠Updated frontend

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,17 +15,29 @@ int main()
 	RenderWindow window(VideoMode(1024, 720), "Bantumi", Style::Close);
 
 	// load background
-	/*
+
 	Texture texture;
-	texture.loadFromFile("./src/background.jpg");
+	texture.loadFromFile("./src/assets/background.jpg");
 	Sprite sprite;
-	Vector2u size = texture.getSize();
-	sprite.setTexture(texture); //This is where you add an & to designate texture as a pointer
-	sprite.setOrigin(size.x / 2, size.y / 2);
-	*/
+	// this is where you add an & to designate texture as a pointer
+	sprite.setTexture(texture);
+	sprite.setOrigin(sprite.getLocalBounds().width / 2, sprite.getLocalBounds().height / 2);
+	float xPos = (float)1024 / 2;
+	float yPos = (float)720 / 2;
+	sprite.setPosition(xPos, yPos);
 
 	// define sockets
 	sf::RectangleShape enemySocket[6], allySocket[6], barnEnemy, barnAlly;
+	// define counter text
+	sf::Text counterText[14];
+
+	// define value of sockets
+	/*int socketValue[14];
+	// start all elements with a value of 6
+	for (int i = 0; i < 14; i++)
+	{
+		socketValue[i] = 6;
+	}*/
 
 	void draw(RenderWindow * window);
 	Event evnt;
@@ -49,12 +61,15 @@ int main()
 		// clear the whole window before rendering a new frame
 		window.clear();
 
+		// draw texture
+		window.draw(sprite);
 		// draw enemy sockets
 		for (int l = 0, pos = 83; l < 6; l++, pos += 150)
 		{
 			enemySocket[l].setSize(sf::Vector2f(100, 100));
 			enemySocket[l].setPosition(sf::Vector2f(pos, 100));
 			enemySocket[l].setFillColor(Color::Blue);
+			enemySocket[l].setOutlineThickness(1.0f);
 			window.draw(enemySocket[l]);
 		}
 
@@ -64,8 +79,34 @@ int main()
 			allySocket[l].setSize(sf::Vector2f(100, 100));
 			allySocket[l].setPosition(sf::Vector2f(pos, 500));
 			allySocket[l].setFillColor(Color::Blue);
+			allySocket[l].setOutlineThickness(1.0f);
 			window.draw(allySocket[l]);
 		}
+
+		// insert text counter in all of the sockets
+		for (int l = 0, pos = 133; l < 6; l++, pos += 150)
+		{
+
+			counterText[l].setString("Stim ");
+			counterText[l].setCharacterSize(16);
+			counterText[l].setPosition(pos, 150);
+			counterText[l].setFillColor(sf::Color::White);
+			counterText[l].setStyle(sf::Text::Bold | sf::Text::Underlined);
+			window.draw(counterText[l]);
+		}
+
+		// draw barns
+		barnEnemy.setSize(sf::Vector2f(100, 100));
+		barnEnemy.setPosition(sf::Vector2f(83, 300));
+		barnEnemy.setFillColor(Color::Red);
+		barnEnemy.setOutlineThickness(1.0f);
+		window.draw(barnEnemy);
+
+		barnAlly.setSize(sf::Vector2f(100, 100));
+		barnAlly.setPosition(sf::Vector2f(833, 300));
+		barnAlly.setFillColor(Color::Red);
+		barnAlly.setOutlineThickness(1.0f);
+		window.draw(barnAlly);
 
 		// end the current frame and display its contents on screen
 		window.display();
