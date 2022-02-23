4d22bd50f7a3e1125db17979ac5a03e7df880f5b
Nicola Preda <preda.nicola.studente@itispaleocapa.it>
2022-02-23 23:39:31 +0100

    🔨Optimized frontend code

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,9 @@
 #include "Platform/Platform.hpp"
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
 using namespace util;
 using namespace std;
 using namespace sf;
@@ -14,8 +18,11 @@ int main()
 	// create window
 	RenderWindow window(VideoMode(1024, 720), "Bantumi", Style::Close);
 
-	// load background
+	// inititialize time random seed
+	srand(time(NULL));
+	int eachBean = rand() % 6 + 3;
 
+	// load background
 	Texture texture;
 	texture.loadFromFile("./src/assets/background.jpg");
 	Sprite sprite;
@@ -27,7 +34,7 @@ int main()
 	sprite.setPosition(xPos, yPos);
 
 	// define sockets
-	sf::RectangleShape enemySocket[6], allySocket[6], barnEnemy, barnAlly;
+	sf::RectangleShape socket[14];
 	// define counter text
 	sf::Text counterText[14];
 	// define font
@@ -35,7 +42,7 @@ int main()
 	font.loadFromFile("./src/assets/fonts/Roboto-Black.ttf");
 
 	// define value of sockets
-	/*int socketValue[14];
+	/*int socketValue[13];
 	// start all elements with a value of 6
 	for (int i = 0; i < 14; i++)
 	{
@@ -63,38 +70,47 @@ int main()
 		}
 		// clear the whole window before rendering a new frame
 		window.clear();
-
 		// draw texture
 		window.draw(sprite);
-		// draw enemy sockets
-		for (int l = 0, pos = 83; l < 6; l++, pos += 150)
-		{
-			enemySocket[l].setSize(sf::Vector2f(100, 100));
-			enemySocket[l].setPosition(sf::Vector2f(pos, 100));
-			enemySocket[l].setFillColor(Color::Blue);
-			enemySocket[l].setOutlineThickness(1.0f);
-			window.draw(enemySocket[l]);
-		}
 
-		// draw ally sockets
-		for (int l = 0, pos = 83; l < 6; l++, pos += 150)
+		// draw sockets, barns and bean counters
+		for (int l = 0, pos = 83; l < 14; l++, pos += 150)
 		{
-			allySocket[l].setSize(sf::Vector2f(100, 100));
-			allySocket[l].setPosition(sf::Vector2f(pos, 500));
-			allySocket[l].setFillColor(Color::Blue);
-			allySocket[l].setOutlineThickness(1.0f);
-			window.draw(allySocket[l]);
-		}
 
-		// insert text counter in all of the sockets
-		for (int l = 0; l < 6; l++)
-		{
-			counterText[l].setFont(font); // font is a sf::Font
+			socket[l].setSize(sf::Vector2f(100, 100));
+			socket[l].setFillColor(Color::Blue);
+			socket[l].setOutlineThickness(1.0f);
 
-			counterText[l].setString("1");
+			if (l < 6)
+			{
+				// draw enemy sockets
+				socket[l].setPosition(Vector2f(pos, 100));
+			}
+			else if (l < 13 && l > 6)
+			{
+				// draw ally sockets
+				socket[l].setPosition(Vector2f(pos, 500));
+			}
+			else if (l == 6)
+			{
+				// draw enemy barn
+				socket[l].setPosition(Vector2f(83, 300));
+				pos = -67;
+			}
+			else if (l == 13)
+			{
+				// draw ally barn
+				socket[l].setPosition(Vector2f(833, 300));
+			}
+
+			window.draw(socket[l]);
+
+			// counters settings
+			counterText[l].setFont(font);
+			counterText[l].setString(to_string(eachBean));
 			counterText[l].setCharacterSize(20);
 
-			sf::FloatRect rectBounds = enemySocket[l].getGlobalBounds();
+			sf::FloatRect rectBounds = socket[l].getGlobalBounds();
 			sf::FloatRect textBounds = counterText[l].getGlobalBounds();
 
 			counterText[l].setPosition(
@@ -104,36 +120,7 @@ int main()
 			counterText[l].setFillColor(sf::Color::White);
 			window.draw(counterText[l]);
 		}
-		for (int l = 0; l < 6; l++)
-		{
-			counterText[l].setFont(font); // font is a sf::Font
 
-			counterText[l].setString("1");
-			counterText[l].setCharacterSize(20);
-
-			sf::FloatRect rectBounds = allySocket[l].getGlobalBounds();
-			sf::FloatRect textBounds = counterText[l].getGlobalBounds();
-
-			counterText[l].setPosition(
-				rectBounds.left + (rectBounds.width / 2) - (textBounds.width / 2),
-				rectBounds.top + (rectBounds.height / 2) - textBounds.height);
-
-			counterText[l].setFillColor(sf::Color::White);
-			window.draw(counterText[l]);
-		}
-
-		// draw barns
-		barnEnemy.setSize(sf::Vector2f(100, 100));
-		barnEnemy.setPosition(sf::Vector2f(83, 300));
-		barnEnemy.setFillColor(Color::Red);
-		barnEnemy.setOutlineThickness(1.0f);
-		window.draw(barnEnemy);
-
-		barnAlly.setSize(sf::Vector2f(100, 100));
-		barnAlly.setPosition(sf::Vector2f(833, 300));
-		barnAlly.setFillColor(Color::Red);
-		barnAlly.setOutlineThickness(1.0f);
-		window.draw(barnAlly);
 		// end the current frame and display its contents on screen
 		window.display();
 	}
