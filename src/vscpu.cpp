0b86dbfb982ad5f38ec6c69ecdf06132dc62bb90
Nicola Preda <preda.nicola.studente@itispaleocapa.it>
2022-03-01 08:02:44 +0100

    Updated vsCPU

--- src/vscpu.cpp
+++ src/vscpu.cpp
@@ -11,17 +11,55 @@ using namespace util;
 using namespace std;
 using namespace sf;
 
-int cpuBrain(int sockets)
+int cpuBrain(int sockets[14])
 {
-	int counter = 0;
-	for (int i = 7; i < 13; i++)
+	int counterAlly = 0;
+	int counterOpponent = 0;
+	for (int i = 0; i < 6; i++)
 	{
-		// check if there is a move that allows to steal beans from the opponent
-		int counter = sockets[i];
-		if (sockets[i + counter] == 0 && sockets[])
+		// i == contatore da 0 a 6
+		// counter == prende il valore contenuto nell'array socket
+		// socket[i + counter] == prende il valore dell'array che si trova se l'array i viene sommato
+		// i + counter == prende il valore del numero dell'array che si ottiene se viene cliccato il primo
+
+		// predicts whether the opponent will be able to steal the beans
+		for (int x = 7; x < 13; x++)
 		{
+			counterOpponent = sockets[x];
+			if (sockets[x + counterOpponent] == 0 && x + counterOpponent < 13)
+			{
+				// checks if it is possible to prevent the opponent from stealing the beans
+				if (sockets[i + counterOpponent] >= x + counterOpponent)
+				{
+					if (sockets[12 - (x + sockets[x])] > 0)
+					{
+						return i;
+					}
+				}
+				// or it removes the beans that can be stolen
+				else
+				{
+				}
+			}
+		}
+
+		// check if there is a move that allows to steal beans from the opponent
+		counterAlly = sockets[i];
+
+		if (sockets[i + counterAlly] == 0 && i + counterAlly < 6 && sockets[12 - (i + counterAlly)] > 1)
+		{
+			return i;
+		}
+		else if (i + counterAlly == 6)
+		{
+			return i;
+		}
+		else if (i + counterAlly > 5)
+		{
+			return i;
 		}
 	}
+	return 0;
 }
 
 void vscpu()
@@ -93,9 +131,134 @@ void vscpu()
 
 		while (window.pollEvent(evnt))
 		{
+			if (turn == 1)
+			{
+				int l = cpuBrain(socketValue);
+
+				// check if the ultimate bean goes into a 0 socket
+				if (socketValue[l + socketValue[l]] == 0 && l + socketValue[l] > 6 && l + socketValue[l] < 13)
+				{
+					socketValue[13] += socketValue[12 - l];
+					socketValue[12 - (l + socketValue[l])] = 0;
+				}
+
+				for (int i = socketValue[l], c = 1, c1 = 0; socketValue[l] != 0; i++, c++, socketValue[l]--)
+				{
+					if ((l + c) > 13)
+					{
+						if (c1 == 6)
+						{
+							c1++;
+						}
+						socketValue[c1]++;
+						c1++;
+						turn = 0;
+					}
+					else
+					{
+						// check if the ultimate bean goes into the barn
+						socketValue[l + c]++;
+						if (l + c == 13)
+						{
+							turn = 1;
+						}
+						else
+						{
+							turn = 0;
+						}
+					}
+				}
+
+				// check wins
+				int totalValueAlly = 0;
+				int totalValueEnemy = 0;
+				for (int i = 0; i < 6; i++)
+				{
+					totalValueAlly += socketValue[i];
+				}
+				if (totalValueAlly == 0)
+				{
+					cout << "Player 2 wins!\n";
+					cout << "Player 2 wins!\n";
+					sf::Text winText;
+					//center text
+					sf::FloatRect textRect = winText.getLocalBounds();
+					winText.setFont(font);
+					winText.setString("Player 1 wins!");
+					winText.setCharacterSize(20);
+					winText.setOrigin(textRect.left + textRect.width / 2.0f,
+						textRect.top + textRect.height / 2.0f);
+					winText.setPosition(sf::Vector2f(1024 / 2.0f, 720 / 2.0f));
+				}
+				else if (socketValue[6] > (eachBean * 12) / 2)
+				{
+					cout << "Player 2 wins!\n";
+					cout << "Player 2 wins!\n";
+					sf::Text winText;
+					//center text
+					sf::FloatRect textRect = winText.getLocalBounds();
+					winText.setFont(font);
+					winText.setString("Player 1 wins!");
+					winText.setCharacterSize(20);
+					winText.setOrigin(textRect.left + textRect.width / 2.0f,
+						textRect.top + textRect.height / 2.0f);
+					winText.setPosition(sf::Vector2f(1024 / 2.0f, 720 / 2.0f));
+				}
+				else
+				{
+					// set player turn
+
+					playerTurn.setPosition(Vector2f(30, 30));
+
+					window.draw(playerTurn);
+				}
+
+				for (int i = 7; i < 13; i++)
+				{
+					totalValueEnemy += socketValue[i];
+				}
+				if (totalValueEnemy == 0)
+				{
+					cout << "Player 1 wins!\n";
+					sf::Text winText;
+					//center text
+					sf::FloatRect textRect = winText.getLocalBounds();
+					winText.setFont(font);
+					winText.setString("Player 1 wins!");
+					winText.setCharacterSize(20);
+					winText.setOrigin(textRect.left + textRect.width / 2.0f,
+						textRect.top + textRect.height / 2.0f);
+					winText.setPosition(sf::Vector2f(1024 / 2.0f, 720 / 2.0f));
+					window.draw(winText);
+				}
+				else if (socketValue[13] > (eachBean * 12) / 2)
+				{
+					cout << "Player 1 wins!\n";
+					cout << "Player 1 wins!\n";
+					sf::Text winText;
+					//center text
+					sf::FloatRect textRect = winText.getLocalBounds();
+					winText.setFont(font);
+					winText.setString("Player 1 wins!");
+					winText.setCharacterSize(20);
+					winText.setOrigin(textRect.left + textRect.width / 2.0f,
+						textRect.top + textRect.height / 2.0f);
+					winText.setPosition(sf::Vector2f(1024 / 2.0f, 720 / 2.0f));
+				}
+				else
+				{
+					// set player turn
+
+					playerTurn.setPosition(Vector2f(30, 30));
+
+					window.draw(playerTurn);
+				}
+			}
+
 			// manage events
 			switch (evnt.type)
 			{
+
 				case Event::Closed:
 					window.close();
 					break;
@@ -115,7 +278,7 @@ void vscpu()
 									if (l > 6 && l != 13)
 									{
 										// check if the ultimate bean goes into a 0 socket
-										if (socketValue[l + socketValue[l]] == 0 && l + socketValue[l] < 13 && l + socketValue[l] > 6)
+										if (socketValue[l + socketValue[l]] == 0 && l + socketValue[l] > 6 && l + socketValue[l] < 13)
 										{
 											socketValue[13] += socketValue[12 - l];
 											socketValue[12 - (l + socketValue[l])] = 0;
@@ -150,10 +313,6 @@ void vscpu()
 									}
 								}
 							}
-							else
-							{
-								cpuBrain(socketValue);
-							}
 						}
 						// set player turn
 						if (turn == 0)
@@ -178,10 +337,30 @@ void vscpu()
 						if (totalValueAlly == 0)
 						{
 							cout << "Player 2 wins!\n";
+							cout << "Player 2 wins!\n";
+							sf::Text winText;
+							//center text
+							sf::FloatRect textRect = winText.getLocalBounds();
+							winText.setFont(font);
+							winText.setString("Player 1 wins!");
+							winText.setCharacterSize(20);
+							winText.setOrigin(textRect.left + textRect.width / 2.0f,
+								textRect.top + textRect.height / 2.0f);
+							winText.setPosition(sf::Vector2f(1024 / 2.0f, 720 / 2.0f));
 						}
 						else if (socketValue[6] > (eachBean * 12) / 2)
 						{
 							cout << "Player 2 wins!\n";
+							cout << "Player 2 wins!\n";
+							sf::Text winText;
+							//center text
+							sf::FloatRect textRect = winText.getLocalBounds();
+							winText.setFont(font);
+							winText.setString("Player 1 wins!");
+							winText.setCharacterSize(20);
+							winText.setOrigin(textRect.left + textRect.width / 2.0f,
+								textRect.top + textRect.height / 2.0f);
+							winText.setPosition(sf::Vector2f(1024 / 2.0f, 720 / 2.0f));
 						}
 
 						for (int i = 7; i < 13; i++)
@@ -191,10 +370,30 @@ void vscpu()
 						if (totalValueEnemy == 0)
 						{
 							cout << "Player 1 wins!\n";
+							sf::Text winText;
+							//center text
+							sf::FloatRect textRect = winText.getLocalBounds();
+							winText.setFont(font);
+							winText.setString("Player 1 wins!");
+							winText.setCharacterSize(20);
+							winText.setOrigin(textRect.left + textRect.width / 2.0f,
+								textRect.top + textRect.height / 2.0f);
+							winText.setPosition(sf::Vector2f(1024 / 2.0f, 720 / 2.0f));
+							window.draw(winText);
 						}
 						else if (socketValue[13] > (eachBean * 12) / 2)
 						{
 							cout << "Player 1 wins!\n";
+							cout << "Player 1 wins!\n";
+							sf::Text winText;
+							//center text
+							sf::FloatRect textRect = winText.getLocalBounds();
+							winText.setFont(font);
+							winText.setString("Player 1 wins!");
+							winText.setCharacterSize(20);
+							winText.setOrigin(textRect.left + textRect.width / 2.0f,
+								textRect.top + textRect.height / 2.0f);
+							winText.setPosition(sf::Vector2f(1024 / 2.0f, 720 / 2.0f));
 						}
 					}
 					break;
